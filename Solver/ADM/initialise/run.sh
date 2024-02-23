b3ad3591e2883e9bfd88564ab8e92ee3eefd227d
admole <a.mole@imperial.ac.uk>
2024-02-23 11:18:34 +0000

    Removed files that are now generated

--- Solver/ADM/initialise/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-NUM=8
-
-cd "$(dirname "$0")"
-pwd
-
-mpirun -np $NUM ~/Documents/Incompact3d/xcompact3d &> log.x3d
-# ~/Documents/Incompact3d/xcompact3d &> log.x3d
--- Solver/ADM/precursor/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-NUM=8
-
-mpirun -np $NUM ~/Documents/Incompact3d/xcompact3d &>> log.x3d
-# ~/Documents/Incompact3d/xcompact3d &> log.x3d
