dfb44b50ffe0c326caa60a5c6804d35964ee4650
admole <a.mole@imperial.ac.uk>
2024-03-22 14:29:36 +0000

    Set turnine angles to 0 when resetting environment

--- Solver/WF_enviroment.py
+++ Solver/WF_enviroment.py
@@ -131,17 +131,10 @@ class TurbEnv(EnvBase):
         if not self.dummy_update:
             self.adm.restart()
 
-        high_alpha = self.max_angle
-        low_alpha = -self.max_angle
-
         # for non batch-locked envs, the input tensordict shape dictates the number
         # of simulators run simultaneously. In other contexts, the initial
         # random state's shape will depend upon the environment batch-size instead.
-        alpha = (
-            torch.rand((*self.batch_size, self.n_turbs, 1), generator=self.rng, device=self.device)
-            * (high_alpha - low_alpha)
-            + low_alpha
-        )
+        alpha = torch.zeros((*self.batch_size, self.n_turbs, 1), device=self.device)
         observation = torch.zeros((*self.batch_size, self.n_turbs, self.obs_per_turbine), device=self.device)
 
         agent_tds = []
