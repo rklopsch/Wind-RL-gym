b7fc5cd2de73dcf1749e6e10f7fa80dac041c492
admole <a.mole@imperial.ac.uk>
2024-04-17 10:07:04 +0100

    Added pressure plot

--- utils/plots.py
+++ utils/plots.py
@@ -12,37 +12,38 @@ import tqdm
 import os
 
 
-def plot_slice(t, casename, env, ax, mean=False):
-    # ax.clear()
+def plot_slice(t, casename, env, ax, height=90):
+    for a in ax:
+        a.clear()
     path = casename
-    if mean:
-        file = os.path.join(path, f'statistics/umean.dat{env.adm.total_timesteps}')
-    else:
-        file = os.path.join(path, f'data/ux-{t}.bin')
+    fileu = os.path.join(path, f'data/ux-{t}.bin')
+    filep = os.path.join(path, f'data/pp-{t}.bin')
     gridx = np.linspace(0, env.adm.lx, env.adm.nx)
     gridy = np.linspace(0, env.adm.ly, env.adm.ny)
     gridz = np.linspace(0, env.adm.lz, env.adm.nz)
 
-    data = np.fromfile(file, dtype=np.float64)
-    datar = data.reshape((env.adm.nx, env.adm.ny, env.adm.nz), order='F')
-    slice_loc = int(90 / env.adm.ly * env.adm.ny)
-    ax.contourf(gridx/1000, gridz/1000, datar[:, slice_loc, :].T, cmap='Blues_r', levels=100)
-    ax.set_aspect('equal')
-    # adm_file = os.path.join(path, 'adm.ad')
-    # yaws = np.loadtxt(adm_file, usecols=3, skiprows=1, unpack=True)
-    # env.adm.farm.set_yaw(yaws)
-    # env.adm.farm.plot_turbines(ax)
-    ax.set_xlabel(r'$x \; (km)$')
-    ax.set_ylabel(r'$y \; (km)$')
+    u = np.fromfile(fileu, dtype=np.float64)
+    ur = u.reshape((env.adm.nx, env.adm.ny, env.adm.nz), order='F')
+    p = np.fromfile(filep, dtype=np.float64)
+    pr = p.reshape((env.adm.nx, env.adm.ny, env.adm.nz), order='F')
+    slice_loc = int(height / env.adm.ly * env.adm.ny)
+    contour_u = ax[0].contourf(gridx/1000, gridz/1000, ur[:, slice_loc, :].T,
+                               cmap='plasma', levels=100, extend='both', vmin=4, vmax=12)
+    contour_p = ax[1].contourf(gridx/1000, gridz/1000, pr[:, slice_loc, :].T,
+                               cmap='Blues_r', levels=100, vmin=-10, vmax=10)
+    for a in ax:
+        a.set_aspect('equal')
+        a.set_xlabel(r'$x \; (km)$')
+        a.set_ylabel(r'$y \; (km)$')
 
 
 @hydra.main(config_path="../ppo/", config_name="config_ppo", version_base="1.2")
 def main(cfg: "DictConfig"):
 
-    casename = './TEST_DATA/'
+    casename = '/home/amole/Downloads/TEST_8'
 
-    fig, axs = plt.subplots(1, 1,
-                            figsize=(10, 3),
+    fig, axs = plt.subplots(2, 1,
+                            figsize=(10, 6),
                             constrained_layout=True,
                             sharex=True,
                             sharey=True)
@@ -60,10 +61,10 @@ def main(cfg: "DictConfig"):
     env = TurbEnv(params, dummy_update=True)
 
     optimisation_iter = 30
-    iters = tqdm.tqdm(range(1000, 1010), desc="Optimisation Iteration", position=0)
+    iters = tqdm.tqdm(range(129, 1128), desc="Optimisation Iteration", position=0)
 
-    anim = ani.FuncAnimation(fig, partial(plot_slice, casename=casename, env=env, ax=axs, mean=False), frames=iters)
-    anim.save(f'animations/test.mp4', fps=10,)#codec='h263p')
+    anim = ani.FuncAnimation(fig, partial(plot_slice, casename=casename, env=env, ax=axs), frames=iters)
+    anim.save(f'animations/test2.mp4', fps=20, dpi=400)#codec='h263p')
 
     # for i in range(1000, 1010):
     #     print(i)
