7ee2e00b2aa5e9b3d7a45e847c77a55c95220876
user name <mweissen-ai4nz@eidf079-gpu1.vms.os.eidf.epcc.ed.ac.uk>
2024-05-31 11:34:06 +0000

    added a dummy solver and code to launch that from smartsim

---
+++ launch_dummy_run.py
@@ -0,0 +1,51 @@
+import numpy as np
+from smartsim import Experiment
+import time
+from smartredis import Client
+
+
+def launch_database(experiment, port):
+    db = experiment.create_database(port=port, db_nodes=1, interface='lo')
+
+    # generate directories for output files
+    # pass in objects to make dirs for
+    experiment.generate(db, overwrite=True)
+
+    # start the database on interactive allocation and wait until database is launched
+    experiment.start(db, block=True)
+
+    # get the status of the database
+    statuses = experiment.get_status(db)
+    print(f"Status of all database nodes: {statuses}.")
+    print(f"Database started on {db.get_address()}.")
+
+    return db
+
+def launch_solver(experiment):
+    aprun = experiment.create_run_settings(exe="python", exe_args="dummy_solver.py")
+    aprun.set_tasks(1)
+    producer = experiment.create_model("dummy_solver", aprun)
+
+    # create directories for the output files and copy
+    # scripts to execution location inside newly created dir
+    # only necessary if its not an executable (python is executable here)
+    producer.attach_generator_files(to_copy="./Solver/dummy_solver.py")
+
+    experiment.generate(producer, overwrite=True)
+    return producer
+
+
+if __name__ == '__main__':
+    exp = Experiment("launch_dummy_run", launcher="local")
+
+    db_port = 6783
+    db = launch_database(exp, db_port)
+
+    solver_app = launch_solver(exp)
+    exp.start(solver_app, block=False, summary=False)
+
+    # shutdown the database because we don't need it anymore
+    time.sleep(15)
+    exp.stop(db)
+
+    print(exp.summary())
