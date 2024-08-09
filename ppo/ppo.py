247dc0ea63dafc17de8950b37fc9089de1740e9d
user name <mweissen-ai4nz@eidf079-gpu1.vms.os.eidf.epcc.ed.ac.uk>
2024-08-09 15:49:58 +0000

    training rewards are logged again

--- ppo/ppo.py
+++ ppo/ppo.py
@@ -199,20 +199,16 @@ def main(cfg: "DictConfig"):
         )
         # We need to expand the done and terminated to match the reward shape (this is expected by the value estimator)
 
-        """
         # Get training rewards and episode lengths
-        episode_rewards = data["next", "agents", "episode_reward"].sum(-2)
-        episode_rewards = episode_rewards[data["next", "done"]]
-        if len(episode_rewards) > 0:
-            episode_length = data["next", "step_count"][data["next", "done"]]
+        episode_rewards = data["next", "agents", "episode_reward"][data["next", "done"]]
+        episode_length = data["next", "step_count"][data["next", "done"].all(-2)]
+        if len(episode_length) > 0:
             log_info.update(
                 {
                     "train/reward": episode_rewards.mean().item(),
-                    "train/episode_length": episode_length.sum().item()
-                    / len(episode_length),
+                    "train/episode_length": episode_length.sum().item() / len(episode_length),
                 }
             )
-        """
 
         training_start = time.time()
         for j in range(cfg_loss_ppo_epochs):
