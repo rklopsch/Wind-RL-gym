c9f75825fd459575da29070badbd9aaf12d05b8e
admole <a.mole@imperial.ac.uk>
2024-09-09 10:05:52 +0100

    Updated yaw and power plots

--- utils/plots.py
+++ utils/plots.py
@@ -1,14 +1,14 @@
-from functools import partial
+# from functools import partial
 
 import numpy as np
 import pandas as pd
-import torch
+# import torch
 from matplotlib import pyplot as plt
-import matplotlib.animation as ani
-from Solver.WF_enviroment import TurbEnv
-from Solver.farm import Turbine, Farm
+# import matplotlib.animation as ani
+# from Solver.WF_enviroment import TurbEnv
+# from Solver.farm import Turbine, Farm
 import hydra
-import tqdm
+# import tqdm
 # import fonts
 import os
 import re
@@ -41,9 +41,9 @@ def plot_slice(t, casename, env, ax, height=90):
 # def plot_power(t, casename, env, ax):
 
 class TimeSeries:
-    def __init__(self, casename, env):
+    def __init__(self, casename, n_turbs):
         self.casename = casename
-        self.nturbines = env.n_turbs
+        self.nturbines = n_turbs
         self.data = []
 
     def collect_data(self):
@@ -52,24 +52,61 @@ class TimeSeries:
             dat = pd.read_csv(file, sep="\s+|, ", engine='python')
             self.data.append(dat)
 
-    def draw(self, it, ax):
+    def draw_power(self, it, ax):
         total_power = 0
         for turbine in range(self.nturbines):
-            ax.plot(self.data[turbine]['Time'][:it], self.data[turbine]['Power'][:it] / 1e6,
-                    color='k',
-                    alpha=0.3,
-                    label=f'Turbine {turbine + 1}')
+            # ax.plot(self.data[turbine]['Time'][:it], self.data[turbine]['Power'][:it] / 1e6,
+            #         color='k',
+            #         alpha=0.3,
+            #         label=f'Turbine {turbine + 1}')
             total_power += self.data[turbine]['Power'][:it]
-        ax.plot(self.data[turbine]['Time'][:it], total_power / 1e6,
+        ax.plot(self.data[turbine]['Time'][:it], total_power[:it] / 1e6,
                 color='k',
                 alpha=1,
-                label=f'Total')
+                label=f'Reinforcement Learning')
+        ax.plot([self.data[turbine]['Time'][0], self.data[turbine]['Time'][it]], [16.46, 16.46],
+                color='r',
+                label=f'Bayesian Optimisation')
+        ax.plot([self.data[turbine]['Time'][0], self.data[turbine]['Time'][it]], [16.46/1.338, 16.46/1.338],
+                color='b',
+                label=f'Baseline')
         # ax.legend(loc='upper right', ncol=len(cases), columnspacing=0.5, frameon=False)
         ax.set_xlabel('Time (s)')
         ax.set_ylabel('Power (MW)')
         ax.set_xlim(self.data[0]['Time'][0], self.data[0]['Time'].iloc[-1])
         ax.set_ylim(0, max(total_power/1e6))
-        ax.legend(frameon=False)
+        # only use first labels in legend
+        handles, labels = ax.get_legend_handles_labels()
+        handles = handles[:3]
+        labels = labels[:3]
+        ax.legend(handles, labels, frameon=False, ncols=1)
+
+    def draw_yaw(self, it, ax):
+        colors = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22',
+                  '#17becf']
+        for turbine in range(self.nturbines):
+        # for turbine in [0]:
+                ax.plot(self.data[turbine]['Time'][:it], self.data[turbine]['YawAng'][:it],
+                    color=colors[turbine],
+                    alpha=0.1,
+                    label=f'Turbine {turbine + 1}')
+        # ax.legend(loc='upper right', ncol=len(cases), columnspacing=0.5, frameon=False)
+        ax.set_xlabel('Time (s)')
+        ax.set_ylabel(r'$\text{Yaw}_i (^{\circ})$')
+        ax.set_xlim(self.data[0]['Time'].iloc[0], self.data[0]['Time'].iloc[-1])
+        ax.set_ylim(-45, 45)
+
+        # Step 2: only use first labels in legend
+        handles, labels = ax.get_legend_handles_labels()
+        handles = handles[:self.nturbines]
+        labels = labels[:self.nturbines]
+
+        # Adjust the alpha of handles in the legend
+        # for handle in handles:
+        #     handle.set_alpha(1.0)
+
+        # Step 4: Display the Legend
+        ax.legend(handles, labels, frameon=False, ncols=3)
 
 
 def extract_integers_from_filenames(directory):
@@ -86,40 +123,52 @@ def extract_integers_from_filenames(directory):
 @hydra.main(config_path="../ppo/", config_name="config_ppo", version_base="1.2")
 def main(cfg: "DictConfig"):
 
-    casename = './outputs/2024-04-18/14-15-25/RESULTS/TEST_2'
 
-    params = {
-        "n_turbines": cfg.env.turbines,
-        "probes_per_turbine": cfg.env.probes_per_turbine,
-        "turbine_diameter": cfg.env.turbine_diameter,
-        "turbine_spacing": cfg.env.turbine_spacing,
-        "max_yaw_speed": cfg.env.max_yaw_speed,
-        "max_yaw_angle": cfg.env.max_yaw_angle,
-        "dt": cfg.env.steps_per_frame * 0.2,
-        "run_steps": cfg.collector.total_frames * cfg.env.steps_per_frame,
-    }
-    env = TurbEnv(params, dummy_update=True)
+    # params = {
+    #     "n_turbines": cfg.env.turbines,
+    #     "probes_per_turbine": cfg.env.probes_per_turbine,
+    #     "turbine_diameter": cfg.env.turbine_diameter,
+    #     "turbine_spacing": cfg.env.turbine_spacing,
+    #     "max_yaw_speed": cfg.env.max_yaw_speed,
+    #     "max_yaw_angle": cfg.env.max_yaw_angle,
+    #     "dt": cfg.env.steps_per_frame * 0.2,
+    #     "run_steps": cfg.collector.total_frames * cfg.env.steps_per_frame,
+    # }
+    # env = TurbEnv(params, dummy_update=True)
 
     # Plotting velocity and pressure
-    fig, axs = plt.subplots(2, 1,
-                            figsize=(10, 6),
-                            constrained_layout=True,
-                            sharex=True,
-                            sharey=True)
+    # fig, axs = plt.subplots(2, 1,
+    #                         figsize=(10, 6),
+    #                         constrained_layout=True,
+    #                         sharex=True,
+    #                         sharey=True)
+    #
+    # snaps = extract_integers_from_filenames(os.path.join(casename, 'data'))
+    # iters = tqdm.tqdm(snaps, desc="Animation Iteration", position=0)
+    # anim = ani.FuncAnimation(fig, partial(plot_slice, casename=casename, env=env, ax=axs), frames=iters)
+    # anim.save(os.path.join(casename, 'slice.mp4'), fps=20, dpi=400)#codec='h263p')
 
-    snaps = extract_integers_from_filenames(os.path.join(casename, 'data'))
-    iters = tqdm.tqdm(snaps, desc="Animation Iteration", position=0)
-    anim = ani.FuncAnimation(fig, partial(plot_slice, casename=casename, env=env, ax=axs), frames=iters)
-    anim.save(os.path.join(casename, 'slice.mp4'), fps=20, dpi=400)#codec='h263p')
-
-    # Plotting Time series
-    fig, axs = plt.subplots(1, 1,
+    fig_power, axs_power = plt.subplots(1, 1,
                             figsize=(6, 4),
                             constrained_layout=True)
-    series1 = TimeSeries(casename, env)
-    series1.collect_data()
-    series1.draw(-1, axs)
-    fig.savefig(os.path.join(casename, 'power.pdf'))
+    fig_yaw, axs_yaw = plt.subplots(1, 1,
+                            figsize=(6, 4),
+                            constrained_layout=True)
+
+    for env in range(32):
+        casename = f'./training_ppo/WindFarm_{env+1}'
+        print(casename)
+
+        series1 = TimeSeries(casename, cfg.env.turbines)
+        series1.collect_data()
+
+        series1.draw_power(-1, axs_power)
+        fig_power.savefig(os.path.join(casename, 'power.pdf'))
+
+        # Plotting Yaw Time series
+
+        series1.draw_yaw(-1, axs_yaw)
+        fig_yaw.savefig(os.path.join(casename, 'yaw.pdf'))
 
 
 if __name__ == "__main__":
