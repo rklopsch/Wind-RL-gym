ad95221e5522664d2a27a73637c660ce4de809b5
maxweissenbacher <max.weissenbacher@proton.me>
2025-03-12 16:14:44 +0000

    updated lagged correlation code

--- paper_plots/lagged_correlation.py
+++ paper_plots/lagged_correlation.py
@@ -9,6 +9,24 @@ import pandas as pd
 import scipy
 
 
+def time_shift(yaws, shift, turbine):
+    if turbine == 1:
+        if shift > 0:
+            yaws_shifted = [yaws[shift:, 0], yaws[:-shift, 1], yaws[:-shift, 2]]
+        elif shift == 0:
+            yaws_shifted = [yaws[:, 0], yaws[:, 1], yaws[:, 2]]
+        else:
+            yaws_shifted = [yaws[:shift, 0], yaws[-shift:, 1], yaws[-shift:, 2]]
+    elif turbine == 2:
+        if shift > 0:
+            yaws_shifted = [yaws[:-shift, 0], yaws[shift:, 1], yaws[:-shift, 2]]
+        elif shift == 0:
+            yaws_shifted = [yaws[:, 0], yaws[:, 1], yaws[:, 2]]
+        else:
+            yaws_shifted = [yaws[-shift:, 0], yaws[:shift, 1], yaws[-shift:, 2]]
+    return yaws_shifted
+
+
 def find_num_eps_envs(data):
     # detect the number of envs and episodes
     num_episodes = 0
@@ -33,20 +51,7 @@ def extract_angles(data, shift=1, turbine=1):
             # for ep in range(1, 2):
             # for ev in range(1, 4):
             yaws = data[f"alphas_ENV_{ev}_EPISODE_{ep}"]
-            if turbine == 1:
-                if shift > 0:
-                    yaws_shifted = [yaws[shift:, 0], yaws[:-shift, 1], yaws[:-shift, 2]]
-                elif shift == 0:
-                    yaws_shifted = [yaws[:, 0], yaws[:, 1], yaws[:, 2]]
-                else:
-                    yaws_shifted = [yaws[:shift, 0], yaws[-shift:, 1], yaws[-shift:, 2]]
-            elif turbine == 2:
-                if shift > 0:
-                    yaws_shifted = [yaws[:-shift, 0], yaws[shift:, 1], yaws[:-shift, 2]]
-                elif shift == 0:
-                    yaws_shifted = [yaws[:, 0], yaws[:, 1], yaws[:, 2]]
-                else:
-                    yaws_shifted = [yaws[-shift:, 0], yaws[:shift, 1], yaws[-shift:, 2]]
+            yaws_shifted = time_shift(yaws, shift, turbine)
             # yaws_shifted = yaws
             angles.append(yaws_shifted)
     angles = np.asarray(angles).transpose(0, 2, 1)
@@ -120,19 +125,41 @@ if __name__ == '__main__':
         output = f"PAIR {pair} | LAG {best_shift[i]} | CORRELATION {best_shift_value[i]} | PVALUE {best_shift_pvalue[i]}"
         print(output)
 
+    # Fit linear models between the time-shifted signals
+    slopes = []
+    intercepts = []
+    stds = []
+    residuals = []
+    for i, pair in enumerate([(1, 2), (1, 3), (2, 3)]):
+        shift = best_shift[i]
+        angles = extract_angles(rl_data, shift, pair[0])
+        angles_A = angles[f'Angle {pair[0]}']
+        angles_B = angles[f'Angle {pair[1]}']
+        regression = scipy.stats.linregress(angles_A, angles_B)
+        slopes.append(regression.slope)
+        intercepts.append(regression.intercept)
+        std = np.linalg.norm((intercepts[i] + slopes[i] * angles_A) - angles_B) / (np.sqrt(angles_A.size)-1)
+        stds.append(std)
+        residuals.append(np.mean(np.abs((intercepts[i] + slopes[i] * angles_A) - angles_B)))
+        print(f"PAIR {pair} | slope {slopes[i]:.2f} | intercept {intercepts[i]:.2f} | std {stds[i]:.2f} | mean residual {residuals[i]:.5f}")
+
     # Plot correlations with shifted data
     fig, axes = plt.subplots(1, 3, figsize=(6, 2),
                              constrained_layout=True, )
 
+    x = np.arange(-40, 41, 1)
     for i, pair in enumerate([(1, 2), (1, 3), (2, 3)]):
         axes[i].set_aspect('equal')
-        print(f'extracted shift of {best_shift[i]}')
-        print(f'with correlation of {best_shift_value[i]:.3f}')
         shifted_angles = extract_angles(rl_data, best_shift[i], pair[0])
-        print(f'plotting kde {i + 1}')
+        print(f'Plotting KDE for {pair}')
         # sns.kdeplot(data=shifted_angles, x=f"Angle {pair[0]}", y=f"Angle {pair[1]}", ax=axes[i], color='k')
         sns.kdeplot(data=shifted_angles, x=f"Angle {pair[0]}", y=f"Angle {pair[1]}", ax=axes[i], fill=True,
                     cmap='Greys', levels=100)
+        axes[i].plot(x, intercepts[i] + slopes[i] * x, c='k', alpha=0.5)
+        # axes[i].plot(x, intercepts[i] + slopes[i] * x + 1.96 * stds[i], c='k', alpha=0.5, linestyle='--')
+        # axes[i].plot(x, intercepts[i] + slopes[i] * x - 1.96 * stds[i], c='k', alpha=0.5, linestyle='--')
+        axes[i].plot(x, intercepts[i] + slopes[i] * x + stds[i], c='k', alpha=0.5, linestyle='--')
+        axes[i].plot(x, intercepts[i] + slopes[i] * x - stds[i], c='k', alpha=0.5, linestyle='--')
         axes[i].set_xlim(-40, 40)
         axes[i].set_ylim(-40, 40)
         axes[i].grid(alpha=0.3, linestyle=':')
@@ -150,4 +177,5 @@ if __name__ == '__main__':
 
     fig.savefig("shifted_correlation.png", dpi=400)
     fig.savefig("shifted_correlation.pdf")
+    plt.close()
     # plt.show()
