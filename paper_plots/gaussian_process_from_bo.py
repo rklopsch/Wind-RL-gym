c364dfed477ae363a56d15be1e353ea5595d92ed
maxweissenbacher <max.weissenbacher@proton.me>
2025-03-12 18:01:56 +0000

    started writing code (incomplete) to estimate value at any angle using GP

---
+++ paper_plots/gaussian_process_from_bo.py
@@ -0,0 +1,22 @@
+import pickle
+import torch
+from botorch.models import SingleTaskGP
+from botorch.fit import fit_gpytorch_mll
+from gpytorch.mlls import ExactMarginalLogLikelihood
+from botorch.acquisition import UpperConfidenceBound
+
+
+if __name__ == '__main__':
+    with open('../outputs/bo_logs/BO_output.pkl', 'rb') as f:
+        bo_logs = pickle.load(f)
+
+
+    train_x = bo_logs['train_x']
+    train_y = bo_logs['train_y']
+    lower_bound = -40.
+    bounds_diff = 80.
+
+    scaled_x = (train_x - lower_bound) / bounds_diff
+    model = SingleTaskGP(scaled_x, train_y)
+    mll = ExactMarginalLogLikelihood(model.likelihood, model)
+    fit_gpytorch_mll(mll)
